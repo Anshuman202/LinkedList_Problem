ddc6123ba7380f9400c337d4096e59b0dca3ab16
Anshuman202 <asanshuman2@gmail.com>
2023-01-13 22:29:11 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -9,9 +9,14 @@
             Console.WriteLine("Current Size of the list is: " + list.getListSize());
 
             list.insertNode(56);
-            list.insertNode(30);
+
             list.insertNode(70);
             list.printNodes();
+
+            Console.WriteLine("Adding 30 at 1st Location");
+            list.insertNth(30, 1);
+            list.printNodes();
+
         }
     }
 
